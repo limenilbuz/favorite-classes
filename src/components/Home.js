92863002fe5a7741b86559f061f873693d64d17d
Emil Zublingit config  --global user.name Emil <ejzublin@gmail.com>
2022-02-09 15:54:56 -0800

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import Class from './Class'
+import "./Home.css"
+
+function Home(props)
+{
+    const [value, setValue] = useState('');
+    const [favoriteClasses, setClasses] = useState([]);
+
+    const handleChange = (event) => 
+    {
+        setValue(event.target.value);
+
+    }
+
+    const handleSubmit = (event) =>
+    {
+        event.preventDefault();
+        if (!favoriteClasses.includes(value))
+        {
+            setClasses(favoriteClasses.concat(value));
+            setValue('');
+        }
+        console.log(favoriteClasses);
+    }
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Add favorites</label>
+                <input type="text" value={value} onChange={handleChange}></input>
+                <button type="submit">Add your fav!</button>
+            </form>
+            <div className="favs-list">
+                {favoriteClasses.map((favClass) => <Class name={favClass} key={favClass}></Class>)}
+            </div>
+        </div>
+    );
+}
+
+export default Home;
